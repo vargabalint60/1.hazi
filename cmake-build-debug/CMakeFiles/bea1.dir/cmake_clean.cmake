7e26c26b400108df0bdde252e12727fc3ae9b5b6
Varga Bálint <vargabalint60@gmail.com>
2021-03-08 21:38:25 +0100

    kesz hazi feladat

---
+++ cmake-build-debug/CMakeFiles/bea1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bea1.dir/main.cpp.obj"
+  "bea1.exe"
+  "bea1.exe.manifest"
+  "bea1.pdb"
+  "libbea1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bea1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
